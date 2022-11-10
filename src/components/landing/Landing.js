5c31a4e062d6d8057d08b2f83b2cfdb0b2deb7a6
mz-suzin <mzsuzin@gmail.com>
2022-11-10 20:07:47 -0300

    new changes, still far from good

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Rocket from '../images/rocket2.gif';
+import Rocket from '../images/rocket5.gif';
+import './Landing.css';
 
 const Landing = () => {
     return (
         <div>
-            <img src={Rocket} className="App-logo" alt="logo" />
+            <p>Hi! I'm Matheus</p>
+            <img src={Rocket} className="Image" alt="logo" />
         </div>
     ); 
 }
--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -5,27 +5,29 @@ import './NavBar.css'
 
 const NavBar = () => {
     return (
-    <nav class="dt dt--fixed w-100 border-box pa3 ph5-ns bb b--black-10">
-        {/* <!--  LEFT PADDING TO CENTER LOGO  WHEN COLLAPSED --> */}
-        <div class="dtc dn-ns w-third"></div>
-        
-        {/* <!--  LOGO  --> */}
-        <a class="dtc v-mid mid-gray link dim w-third w-25-ns tc tl-ns mb2 mb0-ns" href="#" title="Home">
-            <img src={SirIorek} class="dib w4 h4 br-100" alt="Site Name"/>
-        </a>
-        
-        {/* <!-- LINKS / MENU  --> */}
-        <div id="menu" class="static-ns absolute mt5 mt0-ns left-100 db dtc-ns v-mid w-50 w-100-ns w-75-ns h-80 tr">
-            <a class="link dim dark-gray tr tl-ns f3 db dib-ns mr3 mr4-ns" href="#" title="Home">Home</a>
-            <a class="link dim dark-gray tr tl-ns f3 db dib-ns mr3 mr4-ns" href="#" title="About">About</a>
-            <a class="link dim dark-gray tr tl-ns f3 db dib-ns mr3 mr0-ns" href="#" title="Contact">Contact</a>
+        <div class='container'>
+            <nav class="dt dt--fixed w-100 border-box pa3 ph5-ns bb b--black-10">
+                {/* <!--  LEFT PADDING TO CENTER LOGO  WHEN COLLAPSED --> */}
+                <div class="dtc dn-ns w-third"></div>
+                
+                {/* <!--  LOGO  --> */}
+                <a class="dtc v-mid mid-gray link dim w-third w-25-ns tc tl-ns mb2 mb0-ns" href="#" title="Home">
+                    <img src={SirIorek} class="dib w4 h4 br-100" alt="Site Name"/>
+                </a>
+                
+                {/* <!-- LINKS / MENU  --> */}
+                <div id="menu" class="static-ns absolute mt5 mt0-ns left-100 db dtc-ns v-mid w-50 w-100-ns w-75-ns h-80 tr">
+                    <a class="link dim dark-gray tr tl-ns f2 db dib-ns mr3 mr4-ns" href="#" title="Home">Home</a>
+                    <a class="link dim dark-gray tr tl-ns f2 db dib-ns mr3 mr4-ns" href="#" title="About">About me</a>
+                    <a class="link dim dark-gray tr tl-ns f2 db dib-ns mr3 mr0-ns" href="#" title="Contact">Projects</a>
+                </div>
+                
+                {/* <!--  EXPAND MENU BUTTON  --> */}
+                {/* <a href="#" id="menu-btn" class="dtc tr v-mid dn-ns w-third pr2 p0-ns link dim dark-gray">
+                    Menu
+                </a> */}
+            </nav>
         </div>
-        
-        {/* <!--  EXPAND MENU BUTTON  --> */}
-        {/* <a href="#" id="menu-btn" class="dtc tr v-mid dn-ns w-third pr2 p0-ns link dim dark-gray">
-            Menu
-        </a> */}
-        </nav>
     );
 }
 
